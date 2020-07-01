af0d2462378751567d6aca9211f8489719ebcc6c
Kayla Kingston <kkingston09@gmail.com>
2020-07-01 13:11:14 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -46,12 +46,12 @@ class Dog
   end
   
   def self.new_from_db(row)
-    self.new(name: row[1], breed: [2], id: row[0])
+    self.new(name: row[1], breed: row[2], id: row[0])
   end 
   
-  # def self.new_from_db(row)
-  #   self.new(name: row[1], breed: row[2], id: row[0])
-  # end
+  def self.new_from_db(row)
+    self.new(name: row[1], breed: row[2], id: row[0])
+  end
   
   def self.find_by_id(id)
     sql = "SELECT * FROM  dogs WHERE id= ?"
